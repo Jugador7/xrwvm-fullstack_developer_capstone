ab8db370b5926b84142e409f6c8679201b4dc4a9
Jugador7 <164368680+Jugador7@users.noreply.github.com>
2024-08-21 06:45:46 -0500

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -16,18 +16,18 @@ sentiment_analyzer_url = os.getenv(
 # Add code for get requests to back end
 def get_request(endpoint, **kwargs): 
     params = ""
-    if(kwargs): 
+    if( kwargs ):
         for key, value in kwargs.items():
             params = params + key + "=" + value + "&"
 
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as err:
         # If any error occurs
         print("Network exception occurred")
 
@@ -55,9 +55,7 @@ def post_review(data_dict):
         return response.json()  # Assuming the response is in JSON format
     except requests.exceptions.RequestException as e:
         # Log the specific error
-        logger.error(f"Network exception occurred: {str(e)}")
         return {"status": 500, "message": "Network exception occurred"}
     except ValueError:
         # Handle the case where the response is not valid JSON
-        logger.error("Received a response that is not valid JSON")
         return {"status": 500, "message": "Invalid JSON response"}
